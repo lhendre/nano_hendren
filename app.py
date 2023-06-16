9115ade8edc9191cb176ca2bb2f64c65c28e60a1
Ubuntu <ubuntu@ip-172-31-36-58.us-east-2.compute.internal>
2023-06-16 01:27:57 +0000

    Initial Server: Initial Nano Server is now running. Able to perform basic prompts on shakespear model

--- app.py
+++ app.py
@@ -1,16 +1,22 @@
 from flask import Flask, request
 from inference import Inference
+import uuid
+import time
 
 app = Flask(__name__)
+
+user_abuse = {}
+banned_prompts = {"How do i make a computer virus":True}
 
 @app.route("/completions", methods=['post'])
 def completions():
     model = request.args.get('model')
     prompt = request.args.get('prompt')
     suffix = request.args.get('suffix')
-    max_tokens = request.args.get('max_tokens')
-    n = request.args.get('n')
-    logprobs = request.args.get('logprobs')
+    max_tokens = request.args.get('max_tokens', type=int)
+    temperature = request.args.get('temperature', type=float)
+    n = request.args.get('n', type=int)
+    logprobs = request.args.get('logprobs', type=float)
     echo = request.args.get('echo')
     stop = request.args.get('stop')
     presence_penalty = request.args.get('presence_penalty')
@@ -18,9 +24,52 @@ def completions():
     best_of = request.args.get('best_of')
     logit_bias = request.args.get('logit_bias')
     user = request.args.get('user')
-    i = Inference()
-    r=i.run()
-    print("r",r)
 
-    print("test",model,prompt,suffix,max_tokens)
-    return "Hello, World!"
+    check = abuse_check(user,prompt)
+    if check != False:
+        return check
+
+    i = Inference(start=prompt,num_samples=n,max_new_tokens=max_tokens,temperature=temperature)
+    r = i.run()
+
+    idRequest = uuid.uuid4()
+    ts = time.time()
+    result = {
+        "id": "cmpl-"+str(idRequest),
+        "object": "text_completion",
+        "created": ts,
+        "model":    model,
+        "choices": r,
+        "usage": {
+            "prompt_tokens": "t",
+            "completion_tokens": "t",
+            "total_tokens": "t"
+        }
+    }
+    return result
+
+def abuse_check(user, prompt):
+
+    if user is not None and user_abuse[user]>=10:
+        return {
+            "Status Code":403,
+            "Error": "Banned due to user abuse, please contact admin if you wish to be removed from the banned list"
+        }, 403
+    print("prompt",prompt)
+    print("D",banned_prompts[prompt])
+    if prompt in banned_prompts:
+        
+        if user is not None and user_abuse[user] is None:
+            user_abuse[user]=1
+        elif user is not None:
+            user_abuse[user]+=1
+        return {
+            "Status Code":400,
+            "Error": "Banned Prompt, repeated Banned prompts will result in an API Ban"
+        }
+    return False
+
+     
+
+if __name__ == "__main__":
+    app.run(debug=True, host="0.0.0.0")
--- inference.py
+++ inference.py
@@ -11,45 +11,46 @@ class Inference():
     """ class for preparing an drunning inference """
 
     def __init__(self,
-        init_from = 'resume' # either 'resume' (from an out_dir) or a gpt2 variant (e.g. 'gpt2-xl')
-        out_dir = 'out' # ignored if init_from is not 'resume'
-        start = "\n" # or "<|endoftext|>" or etc. Can also specify a file, use as: "FILE:prompt.txt"
-        num_samples = 10 # number of samples to draw
-        max_new_tokens = 500 # number of tokens generated in each sample
-        temperature = 0.8 # 1.0 = no change, < 1.0 = less random, > 1.0 = more random, in predictions
-        top_k = 200 # retain only the top_k most likely tokens, clamp others to have 0 probability
-        seed = 1337
-        device = 'cuda' # examples: 'cpu', 'cuda', 'cuda:0', 'cuda:1', etc.
-        dtype = 'bfloat16' # 'float32' or 'bfloat16' or 'float16'
+        init_from = 'resume', # either 'resume' (from an out_dir) or a gpt2 variant (e.g. 'gpt2-xl')
+        out_dir = 'out-shakespeare-char', # ignored if init_from is not 'resume'
+        start = "\n", # or "<|endoftext|>" or etc. Can also specify a file, use as: "FILE:prompt.txt"
+        num_samples = 10, # number of samples to draw
+        max_new_tokens = 500, # number of tokens generated in each sample
+        temperature = 0.8, # 1.0 = no change, < 1.0 = less random, > 1.0 = more random, in predictions
+        top_k = 200, # retain only the top_k most likely tokens, clamp others to have 0 probability
+        seed = 1337,
+        device = 'cuda', # examples: 'cpu', 'cuda', 'cuda:0', 'cuda:1', etc.
+        dtype = 'bfloat16' if torch.cuda.is_bf16_supported() else 'float16', # 'float32' or 'bfloat16' or 'float16'
         compile = False # use PyTorch 2.0 to compile the model to be faster
     ):
-        self.init_from = init_from
-        self.out_dir = out_dir
-        self.start = start
-        self.num_samples = num_samples
-        self.max_new_tokens = max_new_tokens
-        self.temperature = temperature
-        self.top_k = top_k
-        self.seed = seed
-        self.device = device
-        self.dtype = dtype
-        self.compile = compile
+        self.init_from = init_from if init_from is not None else 'resume'
+        self.out_dir = out_dir if out_dir is not None else 'shakespeare'
+        self.start = start if start is not None else "\n"
+        self.num_samples = num_samples if num_samples is not None else 10
+        self.max_new_tokens = max_new_tokens if max_new_tokens is not None else 500
+        self.temperature = temperature if temperature is not None else .8
+        self.top_k = top_k if top_k is not None else 200
+        self.seed = seed if seed is not None else 1337
+        self.device = device if device is not None else 'cuda'
+        self.dtype = dtype if dtype is not None else 'bfloat16' if torch.cuda.is_bf16_supported() else 'float16'
+        self.compile = compile if compile is not None else False
         exec(open('configurator.py').read()) # overrides from command line or config file
-
+        print("Prompt",start)
         torch.manual_seed(seed)
         torch.cuda.manual_seed(seed)
         torch.backends.cuda.matmul.allow_tf32 = True # allow tf32 on matmul
         torch.backends.cudnn.allow_tf32 = True # allow tf32 on cudnn
-        self.device_type = 'cuda' if 'cuda' in device else 'cpu' # for later use in torch.autocast
+        self.device_type = 'cuda' if 'cuda' in self.device else 'cpu' # for later use in torch.autocast
         self.ptdtype = {'float32': torch.float32, 'bfloat16': torch.bfloat16, 'float16': torch.float16}[dtype]
-        self.ctx = nullcontext() if device_type == 'cpu' else torch.amp.autocast(device_type=device_type, dtype=ptdtype)
+        self.ctx = nullcontext() if self.device_type == 'cpu' else torch.amp.autocast(device_type=self.device_type, dtype=self.ptdtype)
 
-    def run(self, input):
+    def run(self):
+
         # model
-        if init_from == 'resume':
+        if self.init_from == 'resume':
             # init from a model saved in a specific directory
-            ckpt_path = os.path.join(out_dir, 'ckpt.pt')
-            checkpoint = torch.load(ckpt_path, map_location=device)
+            ckpt_path = os.path.join(self.out_dir, 'ckpt.pt')
+            checkpoint = torch.load(ckpt_path, map_location=self.device)
             gptconf = GPTConfig(**checkpoint['model_args'])
             model = GPT(gptconf)
             state_dict = checkpoint['model']
@@ -58,18 +59,18 @@ class Inference():
                 if k.startswith(unwanted_prefix):
                     state_dict[k[len(unwanted_prefix):]] = state_dict.pop(k)
             model.load_state_dict(state_dict)
-        elif init_from.startswith('gpt2'):
+        elif self.init_from.startswith('gpt2'):
             # init from a given GPT-2 model
-            model = GPT.from_pretrained(init_from, dict(dropout=0.0))
+            model = GPT.from_pretrained(self.init_from, dict(dropout=0.0))
 
         model.eval()
-        model.to(device)
+        model.to(self.device)
         if compile:
             model = torch.compile(model) # requires PyTorch 2.0 (optional)
 
         # look for the meta pickle in case it is available in the dataset folder
         load_meta = False
-        if init_from == 'resume' and 'config' in checkpoint and 'dataset' in checkpoint['config']: # older checkpoints might not have these...
+        if self.init_from == 'resume' and 'config' in checkpoint and 'dataset' in checkpoint['config']: # older checkpoints might not have these...
             meta_path = os.path.join('data', checkpoint['config']['dataset'], 'meta.pkl')
             load_meta = os.path.exists(meta_path)
         if load_meta:
@@ -88,19 +89,33 @@ class Inference():
             decode = lambda l: enc.decode(l)
 
         # encode the beginning of the prompt
-        if start.startswith('FILE:'):
-            with open(start[5:], 'r', encoding='utf-8') as f:
-                start = f.read()
-        start_ids = encode(start)
-        x = (torch.tensor(start_ids, dtype=torch.long, device=device)[None, ...])
+        if self.start.startswith('FILE:'):
+            with open(self.start[5:], 'r', encoding='utf-8') as f:
+                self.start = f.read()
+        start_ids = encode(self.start)
+        x = (torch.tensor(start_ids, dtype=torch.long, device=self.device)[None, ...])
+
+# [
+#         {
+#             "text": "\n\nThis is indeed a test",
+#             "index": 0,
+#             "logprobs": null,
+#             "finish_reason": "length"
+#         }
+#         ]
 
         # run generation
-        result = []
+        results = []
         with torch.no_grad():
-            with ctx:
-                for k in range(num_samples):
-                    y = model.generate(x, max_new_tokens, temperature=temperature, top_k=top_k)
+            with self.ctx:
+                for k in range(self.num_samples):
+                    y = model.generate(x, self.max_new_tokens, temperature=self.temperature, top_k=self.top_k)
                     print(decode(y[0].tolist()))
                     print('---------------')
-                    result.append(decode(y[0].tolist()))
-        return result
+                    result={
+                        "text":decode(y[0].tolist()),
+                        "index":k,
+                        "logprobs":None
+                    }
+                    results.append(result)
+        return results
